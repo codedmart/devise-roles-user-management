0eb812ed0d631a685487c875698f4f2d4fbcd4df
Brandon Martin <bmartin@zyphmartin.com>
2010-10-03 16:08:38 -0700

    catch unauthorized requests and redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+    redirect_to root_url
+  end
 end
