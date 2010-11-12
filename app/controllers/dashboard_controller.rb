4a30b37add94924c338bf5035609202f08db53f9
Brandon Martin <bmartin@zyphmartin.com>
2010-11-11 21:57:31 -0800

    fix a pluralization issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @user = User.all.excludes(:id => current_user.id)
+    @users = User.all.excludes(:id => current_user.id)
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,6 +1,6 @@
 class Ability
   include CanCan::Ability
-
+  
   def initialize(user)
     can :manage, :all if user.role == "admin"
   end
